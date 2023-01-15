6aa08db028361944ab3edbf6bb5778015a69d6b2
chain-smok <s1101780@gm.pu.edu.tw>
2023-01-15 14:15:07 +0800

    ruby finish

---
+++ datatype.rb
@@ -0,0 +1,7 @@
+name = "Mike"
+ocucpation = "programmer"
+age = 75
+gpa = 3.2
+ismale = true
+istall = false
+flaws = nil # nil:it has no value
---
+++ string.rb
@@ -0,0 +1,10 @@
+phrase = "Giraffe Academy     "
+puts phrase
+puts phrase.downcase()
+puts phrase.strip()#delete all spaces in string
+puts phrase.length()
+puts phrase.include? "Academys"
+puts phrase[0,3]
+puts phrase.index("ffe")
+
+puts "\""
